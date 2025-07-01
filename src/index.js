2ff47d556f4e9dafe427101ada430184b926133e
mohdabdullahmeraj <mohdabdullahmeraj1705@gmail.com>
2025-07-01 10:59:45 +0530

    Practising middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const responseTime = require('response-time')
 
 const {PORT} = require('./config/serverConfig')
 
@@ -6,7 +8,19 @@ const ApiRouter = require('./routes/api_router')
 
 const app = express()
 
+// app.use(responseTime((req, res, time) => {
+//     console.log("Time elapsed: ", time)
+//     res.setHeader('X-Response-Time', time)
+// }))
+
+app.use(responseTime())
+
+app.use(bodyParser.json()) // application level middleware
+app.use(bodyParser.text()) // application level middleware
+app.use(bodyParser.urlencoded({extended: true})) // application level middleware
+
 app.use('/api', ApiRouter)
+
 
 
 app.listen(PORT, () => {
--- src/routes/v1/ping_router_v1.js
+++ src/routes/v1/ping_router_v1.js
@@ -4,6 +4,24 @@ const { pingController } = require('../../controllers/ping_controller');
 
 const router = express.Router()
 
-router.get('/', pingController)
+const checkRequest = (req, res, next) => {
+    console.log("Request is checked")
+    next()
+    console.log("End Check Request")
+}
+
+const logger = (req, res, next) => {
+    console.log("Logger is checked")
+    next()
+    console.log("End Logger")
+}
+
+const authChecked = (req, res, next) => {
+    console.log("Auth is checked")
+    next()
+    console.log("End Auth")
+}
+
+router.get('/',[checkRequest, logger, authChecked] ,pingController)
 
 module.exports = router
