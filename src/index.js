c3b67f2f059a21647a020f9d8f95cb1ec6fe080c
mohdabdullahmeraj <mohdabdullahmeraj1705@gmail.com>
2025-07-01 17:08:37 +0530

    Manually connected mysql db with express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const responseTime = require('response-time')
+const mysql = require('mysql2')
 
-const {PORT} = require('./config/serverConfig')
+const {PORT, DB_HOST, DB_NAME, DB_PASS, DB_USER} = require('./config/serverConfig')
+
+const db = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME
+})
 
 const ApiRouter = require('./routes/api_router')
 
@@ -25,4 +33,23 @@ app.use('/api', ApiRouter)
 
 app.listen(PORT, () => {
     console.log("Server for shop cart is up")
+
+    db.connect((err) => {
+        if(err){
+            console.log('db did not connect')
+            console.log(err)
+            throw err
+            return
+        }
+
+        console.log('db is connected')
+        db.query('select * from products', (err, result) => {
+            if(err){
+                console.log(err)
+                throw err
+                return
+            }
+            console.log(result)
+        })
+    })
 })
